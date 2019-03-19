aa573dd289d2a9be0aab09448410cb2353199e3c
clarkatron <chrisjclark@gmail.com>
2019-03-19 16:48:45 -0700

    adding inthe dictionary for moment tracking

--- robot_tracking.py
+++ robot_tracking.py
@@ -5,6 +5,8 @@ import imutils
 
 
 cap = cv2.VideoCapture(1)
+board_layout = {}
+
 
 while(1):
 	cap = cv2.VideoCapture(1)
@@ -28,9 +30,17 @@ while(1):
 		if M["m00"] != 0:
 			cX = int(M["m10"] / M["m00"])
 			cY = int(M["m01"] / M["m00"])
+			
 			if len(approx) == 3:
+				if cnt == 0:
+					board_layout[cnt] = (cX, cY)
+				else:
+					if abs((board_layout[cnt-1][0] - cX) + (board_layout[cnt-1][1] - cY)) > 10:
+						board_layout[cnt] = (cX, cY)
+						
 				#cv2.drawContours(nois_reduce, [cnt], 0, (255), 5)
 				cv2.circle(nois_reduce, (cX, cY), 5, (255), -1)
+				
 				#print(str(cX) + ',' + str(cY))
 		else:
 			cX, cY = 0, 0
@@ -38,7 +48,7 @@ while(1):
 	#denoised = cv2.GaussianBlur(edged_frame,(5,5),0)
     #Testing finding triangles:
 	#triangles = find_triangles(edged_frame)
-	
+	print(board_layout)
 	cv2.imshow('Original',frame)
 	cv2.imshow('Contours and edges',nois_reduce)
 	
